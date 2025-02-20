30addd5bea4e225b63ba5b15f7d344ceb890eb2e
Harshet jain <hjain8620.hj@gmail.com>
2025-02-20 19:07:37 +0530

    Update backend.tf

--- tasks/ecs_update/backend.tf
+++ tasks/ecs_update/backend.tf
@@ -5,10 +5,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "ecs-terraform-vasant"
+    bucket         = "ecs-update-terraform-vasant"
     key            = "ecs/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "vasant_ecs"
+    dynamodb_table = "vasant_ecs_update"
     encrypt        = true
   }
 }
