69d75409c8130d8c85ceceaac2d162fabcb4201d
Ankit Kumar <146810887+Kumar22Ankit@users.noreply.github.com>
2025-02-25 12:41:12 +0530

    Update backend.tf

--- tasks/ecs/backend.tf
+++ tasks/ecs/backend.tf
@@ -1,14 +1,14 @@
-provider "aws" {
-  region  = "ap-south-1"
-}
+// provider "aws" {
+ // region  = "ap-south-1"
+// }
 
 
-terraform {
-  backend "s3" {
-    bucket         = "ecs-terraform-vasant"
-    key            = "ecs/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "vasant_ecs"
-    encrypt        = true
-  }
-}
+// terraform {
+  //backend "s3" {
+    //bucket         = "ecs-terraform-vasant"
+    //key            = "ecs/terraform.tfstate"
+    //region         = "ap-south-1"
+    //dynamodb_table = "vasant_ecs"
+    //encrypt        = true
+  //}
+//}
